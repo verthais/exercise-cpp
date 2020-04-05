ab6ccaf5cbb51c018809513291391ee62ad8e9c8
verthais <arcanain@gmail.com>
2020-04-05 10:54:27 +0200

    Improving build.sh

--- README.md
+++ README.md
@@ -5,8 +5,12 @@ This repository is a proving ground for all the online challanges that came my w
 
 ## TODO
 
-[ ] set google test to compile with project
-[ ] add first test
-[ ] run jenkins from docker
-[ ] write pipeline to run tests
-[ ] 
+- [x] set google test to compile with project
+- [x] add first test
+- [ ] run jenkins from docker
+- [ ] write pipeline to run tests
+- [x] add already solved problems
+- [x] add code formating
+- [ ] add git hook for code formatting
+- [ ] start best practices notes
+- [ ] start vim cheat sheet
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 function build()
 {
@@ -34,3 +34,32 @@ function format()
         exit 1
     fi
 }
+
+function help()
+{
+    echo "Usage: ./build.sh [option]"
+    echo ""
+    echo "Run this script in the root folder There is no default options."
+    echo ""
+    echo "Options:"
+    echo "build   <exercise>   build the project. This command might run the project"
+    echo "                     if the executable name is given."
+    echo "clean                cleans the build directory."
+    echo "format               runs clang-format with -style=Webkit on project files."
+    echo "help                 prints this help."
+}
+
+if [[ -z "$_" ]]; then
+    echo "Script is being sourced."
+else
+    case "${1}" in
+    "format") format;;
+    "clean") clean;;
+    "build") build "$2";;
+    "help") help;;
+
+    *) echo "Invalid argument."
+       help
+       exit 1;;
+    esac
+fi
